ed81f65ebb20729f25cbc9d0f3914dd63ec15aad
nilay <nilaypatel913@hotmail.com>
2021-09-14 14:31:14 +0100

    final (further work on design needed)

--- journal/models.py
+++ journal/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 
 class Software(models.Model):
-    SoftwareTag = models.CharField(max_length=50)
+    SoftwareTag = models.CharField(max_length=50, unique=True)
     def __str__(self):
         return self.SoftwareTag
 
@@ -10,4 +10,4 @@ class Resource(models.Model):
     name = models.CharField(max_length=30)
     link = models.URLField(max_length=200, blank=True, null=True)
     software = models.ManyToManyField(Software)
-    attachment = models.FileField(upload_to=".", max_length=100, blank=True, null=True)
+    attachment = models.FileField(upload_to="media/", max_length=100, blank=True, null=True)
